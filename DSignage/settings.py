ec2ae9704dc911181aef1dfeeed7ba8f22e4379d
Pablito <kwolek0@gmail.com>
2016-12-07 20:38:33 +0100

    Changing database to MySQL in DSignage/settings.py

--- DSignage/settings.py
+++ DSignage/settings.py
@@ -76,10 +76,21 @@ WSGI_APPLICATION = 'DSignage.wsgi.application'
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'pacio$adleaf',
+        'HOST': 'pacio.mysql.pythonanywhere-services.com',
+        'PORT': '3306',
+        'USER': 'pacio',
+        'PASSWORD': 'rodzynkiwczekoladzie93',
     }
 }
 
